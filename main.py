717cb490d6704c16caab85886244c7de78935013
aLavaGolem <2278728915@qq.com>
2019-10-21 03:11:34 +0800

    删除文件

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask_uploads import UploadSet, IMAGES, configure_uploads, ALL, TEXT
 from flask import request, Flask, redirect, url_for, render_template, jsonify
 import os
-import json
+import re
+import sqlite3
+from flask import g
+
+
 
 app = Flask(__name__)
 app.config['UPLOADED_PHOTO_DEST'] = os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +27,7 @@ def upload():
     if request.method == 'POST' and 'photo' in request.files:
         print(request.files['photo'])
         filename = photos.save(request.files['photo'],folder='upload')
-        data = {'flag':'00','msg':'成功','filename':filename}
+        data = {'flag':'00','msg':'上传成功','filename':filename}
         return jsonify(data)
     return render_template('upload.html')
 
@@ -34,4 +38,96 @@ def show(name):
     url = photos.url(name)
     return render_template('show.html', url=url, name=name)
 
-app.run()
+@app.route('/parsing')
+def parsing():
+    filename=request.args.get("filename")
+    if filename is None:
+        os.abort(404)
+    filepath=photos.path(filename)
+    dir=readFile(filepath)
+
+    data = {'flag': '00', 'msg': '解析成功', 'filename': filename}
+    return jsonify(data)
+
+
+####DB
+DATABASE = 'DB/binlog.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def update_db(query):
+    db=get_db()
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+    # 执行sql语句
+    cursor.execute(query)
+    # 提交到数据库执行
+    db.commit()
+    cursor.close()
+
+####方法
+#解析文件
+def readFile(filepath):
+    deleteArray = []
+    insertArray =[]
+    updateArray =[]
+    with open(filepath, "r", encoding='UTF-8') as f:
+        str = ""
+        for line in f:
+            if line.__contains__("# at"):
+                arrayAppend(deleteArray,insertArray,updateArray,str)
+                str = ""
+            str += line
+        arrayAppend(deleteArray, insertArray, updateArray, str)
+
+    for i in range(len(deleteArray)):
+        print(deleteArray[i])
+        a=arrayComm(deleteArray[i]);
+        print(a)
+        print(type(a))
+#判断类型添加到数组
+def arrayAppend(arr,arr2,arr3,str):
+    if str.find("DELETE") != -1:
+        arr.append(str)
+    elif str.find("INSERT") != -1:
+        arr2.append(str)
+    elif str.find("UPDATE") != -1:
+        arr3.append(str)
+#数组公共解析
+def arrayComm(str):
+    s=re.search('# at (\d+)',str)
+    # 开始节点
+    start =s.group(1)
+    s=re.search('end_log_pos (\d+)',str)
+    # 结束节点
+    end =s.group(1)
+    s = re.search('(\w+)\s\w{2}:\w{2}:\w{2}', str)
+    #时间
+    time = s.group()
+    date=s.group(1)
+    prefix="20"+date[0:2]+"-"+date[2:4]+"-"+date[4:6]
+    print(prefix)
+    time=time.replace(date,prefix)
+    s = re.search('###[\s\S]+', str)
+    #sql
+    sql = s.group().replace("###", "")
+    return {'start':start,'end':end,'time':time,'sql':sql}
+
+if __name__ == '__main__':
+    app.run()
